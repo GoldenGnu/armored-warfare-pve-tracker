6fb98b6f7de923ebe5edfc7ceb392eec223e4891
GoldenGnu <nkr@niklaskr.dk>
2021-01-23 15:14:32 +0100

    Save rotation_lists.ini

--- admin/save.php
+++ admin/save.php
@@ -7,7 +7,7 @@
 	$file = file('../data.js');
 	$filename = substr($file[0], 2, 16);
 	copy("../data.js","../backup/" . $filename . ".js");
-	//Write new data
+	//Write data.js
 	$myfile = fopen("../data.js", "w");
 	fwrite($myfile, "//" . date("Y-m-d_h-i") . "\r\n");
 	fwrite($myfile, "var missions = [\r\n");
@@ -29,13 +29,38 @@
 	}
 	fwrite($myfile, "];\r\n");
 	$minutes = filter_input(INPUT_POST, 'minutes');
+	if (!is_numeric($minutes) || $minutes < -60 || $minutes > 60 ) {
+		$minutes = 0;
+	}
 	fwrite($myfile, "var minutesOffset = $minutes;\r\n");
 	$seconds = filter_input(INPUT_POST, 'seconds');
+	if (!is_numeric($seconds) || $seconds < -60 || $seconds > 60 ) {
+		$seconds = 0;
+	}
 	fwrite($myfile, "var secondsOffset = $seconds;\r\n");
 	fwrite($myfile, "var date = '". date("Y-m-d h:i") ."';\r\n");
 	fwrite($myfile, "var editor = '". $_SERVER['PHP_AUTH_USER'] ."';\r\n");
 	fclose($myfile);
 
+	//Write rotation_lists.ini
+	$inifile = fopen("../rotation_lists.ini", "w");
+	fwrite($inifile, "#" . date("Y-m-d_h-i") . "\r\n");
+	for ($column = 1; $column <= 6; $column++) {
+		switch ($column) {
+			case 1: fwrite($inifile, "[T1-3 Standard]\r\n"); break;
+			case 2: fwrite($inifile, "[T1-2 Hardcore]\r\n"); break;
+			case 3: fwrite($inifile, "[T4-6 Standard]\r\n"); break;
+			case 4: fwrite($inifile, "[T3-6 Hardcore]\r\n"); break;
+			case 5: fwrite($inifile, "[T7-10 Standard]\r\n"); break;
+			case 6: fwrite($inifile, "[T7-10 Hardcore]\r\n"); break;
+		}
+		for ($row = 1; $row <= 10; $row++) {
+			$id = 'mission-' . $row . '-' . $column;
+			$data = filter_input(INPUT_POST, $id);
+			fwrite($inifile, $row."=". $data."\r\n");
+		}
+	}
+	fclose($inifile);
 	//Done
 	header("Location: /admin");
 	die();
