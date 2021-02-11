5fb2ca5309b123daecaf5387de1110bea9fa319d
GoldenGnu <nkr@niklaskr.dk>
2021-02-11 20:12:47 +0100

    Moved Russian translation to lang/ru.js
    Added GUI elements

---
+++ lang/ru.js
@@ -0,0 +1,53 @@
+var language_ru = {
+//Missions
+	"albatross":"Снегирь",
+	"anvil":"Прибой",
+	"banshee":"Банши",
+	"basilisk":"Василиск",
+	"cavalry":"Скарабей",
+	"cerberus":"Цербер",
+	"dire wolf":"Крепость",
+	"erebos":"Икар",
+	"frostbite":"Буран",
+	"ghost hunter":"Призрак",
+	"harbinger":"Буревестник",
+	"hydra":"Гидра",
+	"kodiak":"Кадьяк",
+	"leviathan":"Левиафан",
+	"life jacket":"Источник",
+	"meltdown":"Полураспад",
+	"onyx":"Оникс",
+	"perseus":"Пламя",
+	"phalanx":"Фаланга",
+	"prometheus":"Прометей",
+	"quarterback":"Прорыв",
+	"raiding party":"Гефест",
+	"red opossum":"Демон",
+	"ricochet":"Рикошет",
+	"rolling thunder":"Гром",
+	"sapphire":"Порох",
+	"scorpio":"Скорпион",
+	"snake bite":"Кобра",
+	"spearhead":"Клин",
+	"starry night":"Звезда",
+	"stormy winter":"Метель",
+	"tiger claw":"Коготь тигра",
+	"tsunami":"Цунами",
+	"umbrella":"Туман",
+	"watchdog":"Стена",
+	"wildfire":"Чистое небо",
+	"zero hour":"Торнадо",
+//GUI
+   "start":"Start", //Header
+   "time":"Time", //Header
+   //Other
+   "active":"Active", //Active Row
+   "hardcore":"Hardcore", //Header & Mission Popup & Settings
+   "standard":"Standard", //Header & Mission Popup & Settings
+   "operation":"Operation", //Mission Popup
+   "updated":"Updated", //Footer
+   "by":"by", //Footer
+   "offset minutes":"Offset Minutes", //Settings
+   "offset seconds":"Offset Seconds", //Settings
+   "language":"Language" //Settings
+};
