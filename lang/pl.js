a9a92f203a9e4505cb652d44c0e8b79fe646f4d0
GoldenGnu <nkr@niklaskr.dk>
2021-02-12 23:07:08 +0100

    Added Polish mission translations

---
+++ lang/pl.js
@@ -0,0 +1,53 @@
+var language_pl = {
+//Missions
+	"albatross":"Albatros",
+	"anvil":"Kowadło",
+	"banshee":"Banshee",
+	"basilisk":"Bazyliszek",
+	"cavalry":"Kawaleria",
+	"cerberus":"Cerber",
+	"dire wolf":"Stalowy Wilk",
+	"erebos":"Ereb",
+	"frostbite":"Odmrożenie",
+	"ghost hunter":"WIDMO",
+	"harbinger":"Zły Omen",
+	"hydra":"Hydra",
+	"kodiak":"Kodiak",
+	"leviathan":"Lewiatan",
+	"life jacket":"Kamizelka Ratunkowa",
+	"meltdown":"Awaria reaktora",
+	"onyx":"Onyks",
+	"perseus":"Perseusz",
+	"phalanx":"FALANGA",
+	"prometheus":"Prometeusz",
+	"quarterback":"Przedarcie",
+	"raiding party":"Raiding Party",
+	"red opossum":"Czerwony Opo",
+	"ricochet":"RYKOSZET",
+	"rolling thunder":"Kulisty Piorun",
+	"sapphire":"Szafir",
+	"scorpio":"Skorpion",
+	"snake bite":"Kobra",
+	"spearhead":"KLIN",
+	"starry night":"Gwiaździsta Noc",
+	"stormy winter":"Burzliwa Zima",
+	"tiger claw":"Pazur tygrys",
+	"tsunami":"Tsunami",
+	"umbrella":"PARASOL",
+	"watchdog":"Watchdog",
+	"wildfire":"Niekontrolowany ogień",
+	"zero hour":"Godzina zero",
+//GUI
+   "start":"Start", //Header
+   "time":"Time", //Header
+   //Other
+   "active":"Active", //Active Row
+   "hardcore":"Hardcore", //Header & Mission Popup & Settings
+   "standard":"Standard", //Header & Mission Popup & Settings
+   "operation":"Operacja", //Mission Popup
+   "updated":"Updated", //Footer
+   "by":"by", //Footer
+   "offset minutes":"Offset Minutes", //Settings
+   "offset seconds":"Offset Seconds", //Settings
+   "language":"Language" //Settings
+};
